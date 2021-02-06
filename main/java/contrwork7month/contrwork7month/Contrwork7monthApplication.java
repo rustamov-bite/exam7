b28312c306eedf7ff77c96a8d18404fb1966041c
rustamov-bite <assasins_murza006@mail.ru>
2021-02-06 11:08:53 +0600

    init project

---
+++ main/java/contrwork7month/contrwork7month/Contrwork7monthApplication.java
@@ -0,0 +1,13 @@
+package contrwork7month.contrwork7month;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Contrwork7monthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Contrwork7monthApplication.class, args);
+    }
+
+}
---
+++ test/java/contrwork7month/contrwork7month/Contrwork7monthApplicationTests.java
@@ -0,0 +1,13 @@
+package contrwork7month.contrwork7month;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Contrwork7monthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
